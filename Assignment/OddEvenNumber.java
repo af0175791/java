88778b5f3dca34df7eeddc0e08d85b4ce2bf6009
af0175791 <103167260+af0175791@users.noreply.github.com>
2022-04-10 14:53:43 -0700

    Add files via upload

---
+++ Assignment/OddEvenNumber.java
@@ -0,0 +1,19 @@
+public class OddEvenNumber{
+public static void main(String args[]){
+int arr[]={6,2,3,5};
+ 
+System.out.println("Odd Numbers are:...");
+for(int i=0;i<arr.length;i++){
+if(arr[i]%2!=0){
+ System.out.println(arr[i] + " ");
+}
+}
+System.out.println("Even Numbers are:...");
+for(int i=0;i<arr.length;i++){
+if(arr[i]%2!=0){
+ System.out.println(arr[i] + " ");
+}
+}
+}
+}
+
