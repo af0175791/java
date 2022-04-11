e4edd123cb8c9c7ae568f632cbc2a794c5b95e4b
af0175791 <103167260+af0175791@users.noreply.github.com>
2022-04-11 10:25:36 -0700

    Add files via upload

---
+++ Practical/CommanndLineExample.java
@@ -0,0 +1,10 @@
+class CommanndLineExample{
+
+public static void main(String args[]){
+//System.out.println("Hello my name is"+args[0]);
+
+for(int i=0;i<args.length;i++){
+System.out.print(args[i]);
+}
+}
+}
