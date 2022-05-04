71cccf03fc3df5760b29b30d6d1d1d07fc9b0c20
af0175791 <103167260+af0175791@users.noreply.github.com>
2022-05-03 21:08:44 -0700

    Add files via upload

---
+++ Practical/NestedException.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NestedException {
+
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+System.out.println("Enter two numbers:");
+try {
+int x=s.nextInt();
+int y=s.nextInt();
+System.out.println(x/y);
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
+}
