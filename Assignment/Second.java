a890d0cc4946f1671102895fe84532090f341b23
af0175791 <103167260+af0175791@users.noreply.github.com>
2022-05-24 01:21:28 -0700

    Add files via upload

---
+++ Assignment/Second.java
@@ -0,0 +1,9 @@
+public class  SecondProgram{
+public static void main(String[] args) {
+String str1 = "Stephen Edwin King";
+String str2 = "Walter Winchell";
+String str3 = "stephen edwin king";
+System.out.println("\""+ str1 +"\""+" equals "+"\""+ str2 +"\""+"? "+str1.equalsIgnoreCase(str2));
+System.out.println("\""+ str1 +"\""+" equals "+"\""+ str3 +"\""+"? "+str1.equalsIgnoreCase(str3));
+}
+}
